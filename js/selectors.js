1278c97ffcc93c0e57ea04a9ae3e06fd22494520
Darren Paddock <darrenleepaddock@gmail.com>
2018-02-14 22:27:03 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,15 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last');
 }
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
